e4a43c030060cb33c756e121245394ad91ee58d5
CyrilGremaud <cy.gremaud@gmail.com>
2022-05-02 17:09:57 +0200

    added traces

--- randomTrack.py
+++ randomTrack.py
@@ -14,19 +14,22 @@ def ranTrack():
         points.append([int(random.random() * x), int(random.random() * y)])
 
 
-    for s in range(1000):
-        for i in range(num_points):
-            p = points[(i + len(points)) % len(points)]
-            p1 = points[(i + 1 + len(points)) % len(points)]
-            p2 = points[(i - 1 + len(points)) % len(points)]
-            points[i] = [int((p[0] + p1[0] + p2[0]) / 3), int((p[1] + p1[1] + p2[1]) / 3)]
+    
 
 
-        track = Track(points, width)
-        render = Render(track, 1000, 500)
-        render.render()
-        for i in range(10):
-            render.show()
+    # for s in range(1000):
+    #     for i in range(num_points):
+    #         p = points[(i + len(points)) % len(points)]
+    #         p1 = points[(i + 1 + len(points)) % len(points)]
+    #         p2 = points[(i - 1 + len(points)) % len(points)]
+    #         points[i] = [int((p[0] + p1[0] + p2[0]) / 3), int((p[1] + p1[1] + p2[1]) / 3)]
+
+
+    #     track = Track(points, width)
+    #     render = Render(track, 1000, 500)
+    #     render.render()
+    #     for i in range(10):
+    #         render.show()
     
 
 
